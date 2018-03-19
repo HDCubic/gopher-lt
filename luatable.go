e021b7bfccfdcbb2c94757e55a51c316d4f6491b
HDCubic <1129187334@qq.com>
2018-03-20 00:55:10 +0800

    add lua.LTable binding with tag \`lt:"fieldname"\`

---
+++ luatable.go
@@ -0,0 +1,48 @@
+package lt
+
+import (
+	"reflect"
+
+	lua "github.com/yuin/gopher-lua"
+)
+
+//toLValue 转换成LValue并判断是否为空
+func toLValue(L *lua.LState, value reflect.Value) (lua.LValue, bool) {
+	switch value.Kind() {
+	case reflect.String:
+		return lua.LString(value.String()), value.Len() == 0
+	case reflect.Bool:
+		return lua.LBool(value.Bool()), !value.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return lua.LNumber(value.Int()), value.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return lua.LNumber(value.Uint()), value.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return lua.LNumber(value.Float()), value.Float() == 0
+	case reflect.Slice:
+		v := L.NewTable()
+		for i := 0; i < value.Len(); i++ {
+			lv, _ := toLValue(L, value.Index(i))
+			v.RawSet(lua.LNumber(i), lv)
+		}
+		return v, value.Len() == 0
+	case reflect.Interface, reflect.Ptr:
+		return lua.LNil, false
+	}
+	return NewLTable(L, value.Interface()), true
+}
+
+// NewLTable 从value生成一个lua table
+func NewLTable(L *lua.LState, value interface{}) *lua.LTable {
+	t := L.NewTable()
+	vt := reflect.TypeOf(value)
+	vv := reflect.ValueOf(value)
+	for i := 0; i < vt.NumField(); i++ {
+		f := vt.Field(i)
+		name := f.Tag.Get("lt")
+		if v, yes := toLValue(L, vv.Field(i)); !yes {
+			t.RawSet(lua.LString(name), v)
+		}
+	}
+	return t
+}
---
+++ table_test.go
@@ -0,0 +1,45 @@
+package lt
+
+import (
+	"testing"
+
+	lua "github.com/yuin/gopher-lua"
+	luajson "layeh.com/gopher-json"
+)
+
+type Demo struct {
+	ResourceID string  `json:"resource_id" lt:"resource_id"`
+	Value      int64   `json:"value" lt:"value"`
+	IsEnable   bool    `json:"is_enable" lt:"is_enable"`
+	FValue     float64 `json:"f_value" lt:"f_value"`
+}
+
+// Hello 测试方法
+func Hello(L *lua.LState) int {
+	value := Demo{
+		ResourceID: "3121412",
+		Value:      123214,
+		IsEnable:   true,
+		FValue:     1.23,
+	}
+	table := NewLTable(L, value)
+	L.Push(table)
+	return 1 /* number of results */
+}
+
+func TestNewLTable(t *testing.T) {
+	L := lua.NewState()
+	defer L.Close()
+	luajson.Preload(L)
+	L.SetGlobal("hello", L.NewFunction(Hello))
+	if err := L.DoString(`
+	json = require("json")
+	t = hello()
+	for k, v in pairs(t) do
+		print(k, v)
+	end
+	print(json.encode(t))
+	`); err != nil {
+		panic(err)
+	}
+}
